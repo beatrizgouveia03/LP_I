d249b7ce1b589ea5042ba061518f4a58f53e527d
beatrizgouveia03 <beatrizgouveia.gadelha0@gmail.com>
2023-04-08 10:57:56 -0300

    Questão 1 concluída

--- atividade_I/questao_1/include/utilities.h
+++ atividade_I/questao_1/include/utilities.h
@@ -1,6 +1,7 @@
 #include <vector>
 #include <cstring>
 #include <sstream>
+#include <iomanip>
 #include <iostream>
 
 #include <Funcionario.h>
@@ -20,7 +21,7 @@ void tratarEntrada(T &var)
 std::vector<Funcionario> configuracaoInicial()
 {
     std::string nome[] = {"Ana Clara Queiroz", "Matheus França", "Marcia Maria", "João Marcelo", "Beatriz Santana", "Gustavo Homero"};
-    std::string departamento[] = {"Financeiro", "RH", "Vendas"};
+    std::string departamento[] = {"FINANCEIRO", "RH", "VENDAS"};
     double salario[] = {1500., 2750., 1000.};
     std::vector<Funcionario> base;
 
@@ -40,36 +41,70 @@ std::vector<Funcionario> configuracaoInicial()
     return base;
 }
 
-void cadastrarFuncionario(std::vector<Funcionario> *funcionarios)
+void cadastrarFuncionario(std::vector<Funcionario> *funcionarios, std::vector<std::string> *departamentos )
 {
     Funcionario novo;
     std::string entrada;
     double salario;
+    bool dep = false;
 
     std::cout << "Informe o nome do novo funcionário:\n";
-    tratarEntrada(entrada);
+    getline(std::cin, entrada);
     novo.setNome(entrada);
 
-    std::cout << "Informe o departamento:\n";
+    std::cout << "Informe o departamento:(Em letras maiúsculas apenas)\n"; 
+    for(std::string d : *departamentos){
+        std::cout << " - " << d << std::endl;
+    }
     tratarEntrada(entrada);
     novo.setDepartamento(entrada);
 
+    for (std::string d : *departamentos)
+    {
+        std::string dep = novo.getDepartamento();
+        if (!strcmp(d.c_str(), dep.c_str()))
+        {
+            dep = true;
+            break;
+        }
+    }
+
+    if (!dep)
+    {
+        departamentos->push_back(novo.getDepartamento());
+    }
+
     std::cout << "Informe o salário:\n";
+    for(std::string d : *departamentos){
+        for(Funcionario f :  *funcionarios){
+            std::string dep = f.getDepartamento();
+            if(!strcmp(d.c_str(), dep.c_str())){
+                std::cout << " *" << d << " - R$" << std::fixed << std::setprecision(2) << f.getSalario() << std::endl; 
+                break;
+            }
+        }
+    }
     tratarEntrada(salario);
     novo.setSalario(salario);
 
     funcionarios->push_back(novo);
-    std::cout << "Funcionário cadstrado!\n\n";
+
+   
+    std::cout << "Funcionário cadastrado!\n\n";
 
     return;
 }
 
-void porDepartamento(std::vector<Funcionario> funcionarios)
+void porDepartamento(std::vector<Funcionario> funcionarios, std::vector<std::string> departamentos)
 {
     std::string departamento;
 
-    std::cout << "Digite o nome do departamento:\n";
-    tratarEntrada(departamento);
+    std::cout << "Digite o nome do departamento:(Em letras maiúsculas apenas)\n";
+    for (std::string d : departamentos)
+    {
+        std::cout << " - " << d << std::endl;
+    }
+    tratarEntrada(departamento); 
 
     std::cout << departamento << std::endl;
     for (Funcionario f : funcionarios)
@@ -85,12 +120,16 @@ void porDepartamento(std::vector<Funcionario> funcionarios)
     return;
 }
 
-void aumentoSalario(std::vector<Funcionario> *funcionarios)
+void aumentoSalario(std::vector<Funcionario> *funcionarios, std::vector<std::string> departamentos)
 {
     int porcentagem;
     std::string departamento;
 
     std::cout << "Informe o departamento:\n";
+    for (std::string d : departamentos)
+    {
+        std::cout << " - " << d << std::endl;
+    }
     tratarEntrada(departamento);
 
     std::cout << "Informe o aumento a ser feito:(Por exemplo '15' para um aumento de 15%)\n";
--- atividade_I/questao_1/src/main.cpp
+++ atividade_I/questao_1/src/main.cpp
@@ -3,24 +3,25 @@
 int main()
 {
     std::vector<Funcionario> funcionarios = configuracaoInicial();
+    std::vector<std::string> departamento = {"FINANCEIRO", "RH", "VENDAS"};
     bool fim = false;
     int acao;
 
     while (!fim)
     {
-        std::cout << "Escolha uma acao: \n 1-Cadastrar funcionário\n 2-Obter info de um departamento\n 3-Aumentar o salário de um departamento\n 4-Sair\n";
+        std::cout << "Escolha uma ação:\n 1-Cadastrar funcionário\n 2-Obter info de um departamento\n 3-Aumentar o salário de um departamento\n 4-Sair\n";
         tratarEntrada(acao);
 
         switch (acao)
         {
         case 1:
-            cadastrarFuncionario(&funcionarios);
+            cadastrarFuncionario(&funcionarios, &departamento);
             break;
         case 2:
-            porDepartamento(funcionarios);
+            porDepartamento(funcionarios, departamento);
             break;
         case 3:
-            aumentoSalario(&funcionarios);
+            aumentoSalario(&funcionarios, departamento);
             break;
         case 4:
         default:
