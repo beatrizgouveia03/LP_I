5827d30d4166ed911b7c449724f0b33cc704dbce
Beatriz <72415136+beatrizgouveia03@users.noreply.github.com>
2023-04-18 12:34:51 +0000

    music class implementation

--- menu_musicas/include/Musica.hpp
+++ menu_musicas/include/Musica.hpp
@@ -1,12 +1,24 @@
 #ifndef MUSICA_HPP
 #define MUSICA_HPP
 
+#include <string>
+
+using namespace std;
+
 class Musica
 {
 private:
-    /* data */
+    string titulo;
+    string artista;
 public:
-   /* data */
+   Musica();
+   Musica(string titulo, string artista);
+
+   void setTitulo(string titulo);
+   void setArtista (string artista);
+
+   string getTitulo();
+   string getArtista();
 };
 
 #endif
