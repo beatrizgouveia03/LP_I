7e26a77aedcc46375917d6e4c31be632a4eede0f
beatrizgouveia03 <beatrizgouveia.gadelha0@gmail.com>
2023-04-26 10:29:10 -0300

    tests messages excluded

--- menu_musicas/src/main.cpp
+++ menu_musicas/src/main.cpp
@@ -7,20 +7,7 @@ using namespace std;
 int main()
 {
     Lista<Musica> musicas = configuracaoInicialMusicas();
-    cout << "Musicas configuradas..\n";
     Lista<Playlist> playlists = configuracaoInicialPlaylists(&musicas);
-    cout << "Playlists configuradas..\n";
-
-    cout << endl;
-
-    // Teste baseMusicas
-    cout << "Teste baseMusicas..\n";
-    menuListagemMusicas(&musicas);
-
-    // Teste basePlaylist
-    cout << "Teste basePlaylist..\n";
-    menuListagemPlaylists(&playlists);
-   
 
     menuPrincipal(&musicas, &playlists);
 
