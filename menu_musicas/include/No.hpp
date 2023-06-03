ea3a98df8a8f8ecf2f1772f3bb7e4ff68be92653
Beatriz <beatrizgouveia.gadelha0@gmail.com>
2023-06-03 20:32:30 -0300

    reorganized file

--- menu_musicas/include/No.hpp
+++ menu_musicas/include/No.hpp
@@ -25,66 +25,46 @@ private:
 public:
     /// @brief Construtor padrão da classe
     /// @param NULL
-    No();
+    No()
+    {
+        this->valor = T();
+        this->proximo = nullptr;
+    }
     /// @brief Construtor que inicializa o nó com um valor
     /// @param valor Valor com que o nó será iniciado
-    No(T valor);
+    No(T valor)
+    {
+        this->valor = valor;
+        this->proximo = nullptr;
+    }
 
     /// @brief Função para configurar o valor do nó
     /// @param valor Valor que será atribuído ao nó
-    void setValor(T valor);
+    void setValor(T valor)
+    {
+        this->valor = valor;
+    }
+
     /// @brief Função para configurar o próximo nó
     /// @param proximo Iterador para o próximo nó
-    void setProximo(No<T> *proximo);
+    void setProximo(No<T> *proximo)
+    {
+        this->proximo = proximo;
+    }
 
     /// @brief Função que retorna o valor do nó
     /// @return Valor do nó
-    T getValor();
+    T getValor() const
+    {
+        return valor;
+    }
+
     /// @brief Função que retorna o próximo nó
     /// @return Iterador para o próximo nó
-    No<T> *getProximo();
+    No<T> *getProximo() const
+    {
+        return proximo;
+    }
 };
-
-// Construtor padrão
-template <class T>
-No<T>::No()
-{
-    this->value = T();
-    this->proximo = nullptr;
-}
-
-// Construtor que inicializa o valor do nó
-template <class T>
-No<T>::No(T valor /*Valor a ser inicializado o valor do nó*/)
-{
-    this->valor = valor;
-    this->proximo = nullptr;
-}
-
-// Métodos setters e getters para o atributo valor
-template <class T>
-void No<T>::setValor(T valor)
-{
-    this->valor = valor;
-}
-
-template <class T>
-T No<T>::getValor()
-{
-    return this->valor;
-}
-
-// Métodos setters e getters para o atributo proximo
-template <class T>
-void No<T>::setProximo(No<T> *proximo)
-{
-    this->proximo = proximo;
-}
-
-template <class T>
-No<T> *No<T>::getProximo()
-{
-    return this->proximo;
-}
 
 #endif // NO_HPP
